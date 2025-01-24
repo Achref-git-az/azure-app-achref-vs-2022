dec13a7281357e5fd0c77d6f88014dd319e7439e
sakkat-personal-git <achref.Sakkat@gmail.com>
2025-01-24 18:10:18 +0100

    az db connection ef core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace azure_app_achref.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
